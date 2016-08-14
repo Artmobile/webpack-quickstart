c03195ea88c9e1633327cc046e527f6558abbad8
Ilya Alberton <ilya@iron-buzz.com>
2016-08-14 13:42:35 +0300

    Added CommonsChunkPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,9 @@ module.exports = {
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify(NODE_ENV)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'common'
     })
   ],
 
